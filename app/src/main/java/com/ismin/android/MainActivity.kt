ced4cabd458df228b7ee64b131287ece31fd63d7
Gaëtan Maisse <gaetanmaisse@gmail.com>
2024-11-18 20:51:59 +0100

    feat: implement TP

--- app/src/main/java/com/ismin/android/MainActivity.kt
+++ app/src/main/java/com/ismin/android/MainActivity.kt
@@ -4,12 +4,25 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+const val SERVER_BASE_URL = "https://bookshelf-gme.cleverapps.io"
 
 class MainActivity : AppCompatActivity(), BookCreator {
 
     private val bookshelf = Bookshelf()
+
+    private val retrofit = Retrofit.Builder().addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(SERVER_BASE_URL).build()
+    private val bookService = retrofit.create(BookService::class.java)
+
 
     private val btnCreateBook: FloatingActionButton by lazy {
         findViewById(R.id.a_main_btn_create_book)
@@ -19,9 +32,20 @@ class MainActivity : AppCompatActivity(), BookCreator {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initData()
+        bookService.getAllBooks().enqueue(object : Callback<List<Book>> {
+            override fun onResponse(
+                call: Call<List<Book>>, response: Response<List<Book>>
+            ) {
+                val allBooks: List<Book>? = response.body()
+                allBooks?.forEach { bookshelf.addBook(it) }
+                displayBookListFragment()
+            }
 
-        displayBookListFragment()
+            override fun onFailure(call: Call<List<Book>>, t: Throwable) {
+                Toast.makeText(baseContext, "Something wrong happened", Toast.LENGTH_SHORT).show()
+            }
+        })
+
 
         btnCreateBook.setOnClickListener {
             displayCreateBookFragment()
@@ -43,20 +67,7 @@ class MainActivity : AppCompatActivity(), BookCreator {
         fragmentTransaction.commit()
         btnCreateBook.visibility = View.GONE
     }
-
-    private fun initData() {
-        bookshelf.addBook(
-            Book(
-                "978-2253004226",
-                "Le meilleur des mondes",
-                "Aldous Huxley",
-                "1932-01-01"
-            )
-        )
-        bookshelf.addBook(Book("978-2070413119", "1984", "George Orwell", "1949-06-08"))
-        bookshelf.addBook(Book("978-2070368229", "Fahrenheit 451", "Ray Bradbury", "1953-10-01"))
-    }
-
+    
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
@@ -75,7 +86,16 @@ class MainActivity : AppCompatActivity(), BookCreator {
     }
 
     override fun onBookCreated(book: Book) {
-        bookshelf.addBook(book)
-        displayBookListFragment()
+        bookService.createBook(book)
+            .enqueue {
+                onResponse = {
+                    val bookFromServer: Book? = it.body()
+                    bookshelf.addBook(bookFromServer!!)
+                    displayBookListFragment()
+                }
+                onFailure = {
+                    Toast.makeText(this@MainActivity, it?.message, Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
